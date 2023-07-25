5a89a21efa7216c76a1c35019064788376453e80
DUP3RELE <97246430+DUP3RELE@users.noreply.github.com>
2023-07-25 14:17:50 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,11 +5,11 @@ const cube = document.querySelector(".rotating-cube-container__cube");
 const scrollToTopBtn = document.querySelector("#scrollToTop");
 const scrollObject = document.querySelectorAll(".scroll__hidden");
 
-let mouseX = 0;
-let mouseY = 0;
-let rotationCubeX = 0;
-let rotationCubeY = 0;
-const rotationValue = 200;
+// let mouseX = 0;
+// let mouseY = 0;
+// let rotationCubeX = 0;
+// let rotationCubeY = 0;
+// const rotationValue = 200;
 
 // CRICLE
 const circleText = document.querySelector(".circle__text");
@@ -35,19 +35,49 @@ const handleNav = () => {
 	});
 };
 // CUBE
-const handleMouseMove = (e) => {
-	mouseX = e.clientX;
-	mouseY = e.clientY;
-	rotateX = (mouseY / window.innerHeight - 0.5) * rotationValue;
-	rotateY = (mouseX / window.innerWidth - 0.5) * rotationValue;
-	cube.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-};
+// const handleMouseMove = (e) => {
+// 	mouseX = e.clientX;
+// 	mouseY = e.clientY;
+// 	rotateX = (mouseY / window.innerHeight - 0.5) * rotationValue;
+// 	rotateY = (mouseX / window.innerWidth - 0.5) * rotationValue;
+// 	cube.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+// };
 // Desktop CUBE
-const handleCubeMove = (i) => {
-	if (window.matchMedia("(max-width: 768px)")) {
-		console.log(Date.get);
+function widthCheck(x) {
+	if (x.matches) {
+		let rotationX = 0;
+		let rotationY = 0;
+
+		const rotateCube = () => {
+			rotationX += 1;
+			rotationY += 1;
+			cube.style.transform = `rotateX(${rotationX}deg) rotateY(${rotationY}deg)`;
+		};
+
+		const rotationInterval = setInterval(rotateCube, 30);
+		rotateCube();
+	} else {
+		let mouseX = 0;
+		let mouseY = 0;
+		let rotationCubeX = 0;
+		let rotationCubeY = 0;
+		const rotationValue = 200;
+
+		const handleMouseMove = (e) => {
+			mouseX = e.clientX;
+			mouseY = e.clientY;
+			rotateX = (mouseY / window.innerHeight - 0.5) * rotationValue;
+			rotateY = (mouseX / window.innerWidth - 0.5) * rotationValue;
+			cube.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+		};
+		window.addEventListener("mousemove", handleMouseMove);
 	}
-};
+}
+
+const x = window.matchMedia("(max-width: 1200px)");
+widthCheck(x);
+x.addListener(widthCheck);
+
 // scroll-to-top arrow
 document.addEventListener("DOMContentLoaded", function () {
 	const scrollToTop = document.getElementById("scrollToTop");
